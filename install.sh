c711d542e804ef85a4ac82d092ac75e59a11e531
Luochao <luochao@megvii.com>
2020-10-19 17:53:53 +0800

    Change install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
 WORKSPACE=$(cd $(dirname $0) && pwd)
 cd $WORKSPACE/repos/vimcolors
+
+git submodule update --init
+
 mkdir -p ~/.vim/colors
 for filename in $(ls)
 do
@@ -13,3 +17,17 @@ done
 sudo apt-get install curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# install zsh
+sudo apt-get install zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install pure zsh prompt
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
+echo "fpath+=$HOME/.zsh/pure">>$HOME/.zshrc
+echo "autoload -U promptinit; promptinit">>$HOME/.zshrc 
+echo "prompt pure">>$HOME/.zshrc
+source $HOME/.zshrc
